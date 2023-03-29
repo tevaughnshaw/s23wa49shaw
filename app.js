680c5fb7841b3b8feee461cb453e5e3244da9591
tevaughnshaw <tevaughn.shaw1@gmail.com>
2023-03-28 20:03:22 -0500

    created new endpoint

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var myDataRouter = require('./routes/mydata');
+
+const port = 3000;
 
 var app = express();
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/mydata', myDataRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
